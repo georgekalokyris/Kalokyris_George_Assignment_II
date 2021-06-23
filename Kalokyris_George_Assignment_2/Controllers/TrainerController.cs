8d10c363d6426b76a1c8529c809d5e6a2f41dc1e
George Kalokyris <kalokirisg@gmail.com>
2021-06-23 16:02:22 +0100

    Sorting done

--- Kalokyris_George_Assignment_2/Controllers/TrainerController.cs
+++ Kalokyris_George_Assignment_2/Controllers/TrainerController.cs
@@ -16,10 +16,12 @@ namespace Kalokyris_George_Assignment_2.Controllers
         private MyDatabase db = new MyDatabase();
 
         // GET: Trainer
-        public ActionResult Index(string firstName, string lastName, int? Year, int salary = 0)
+        public ActionResult Index(string firstName, string lastName, int? Year, string sortOrder,int salary = 0)
         {
+            
             var Trainers = db.Trainers.ToList();
 
+            //Filtering
             if (!String.IsNullOrEmpty(firstName))
             {
                 Trainers = Trainers.Where(x => x.FirstName.ToUpper().Contains(firstName.ToUpper())).ToList();
@@ -50,6 +52,31 @@ namespace Kalokyris_George_Assignment_2.Controllers
                 Trainers = Trainers.Where(x => x.HireDate.Year == Year).ToList();
             }
 
+            //Sorting
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.FirstNameSort = String.IsNullOrEmpty(sortOrder) ? "FirstName" : "";
+            ViewBag.FirstNameSort = sortOrder == "FirstName" ? "FirstNameDesc" : "FirstName";
+            ViewBag.LastNameSort = sortOrder == "LastName" ? "LastDesc" : "LastName";
+            ViewBag.Salary = sortOrder == "Salary" ? "SalaryDesc" : "Salary";
+            ViewBag.HireDate = sortOrder == "HireDate" ? "HireDateDesc" : "HireDate";
+            ViewBag.TrainerAvailable = sortOrder == "isAvailable" ? "isAvailableDesc" : "isAvailable";
+
+             switch (sortOrder)
+            {
+                case "FirstName":Trainers = Trainers.OrderBy(x => x.FirstName).ToList(); break;
+                case "FirstNameDesc":Trainers = Trainers.OrderByDescending(x => x.FirstName).ToList(); break;
+                case "LastName": Trainers = Trainers.OrderBy(x => x.LastName).ToList(); break;
+                case "LastDesc": Trainers = Trainers.OrderByDescending(x => x.LastName).ToList(); break;
+                case "Salary": Trainers = Trainers.OrderBy(x => x.Salary).ToList(); break;
+                case "SalaryDesc": Trainers = Trainers.OrderByDescending(x => x.Salary).ToList(); break;
+                case "HireDate": Trainers = Trainers.OrderBy(x => x.HireDate).ToList(); break;
+                case "HireDateDesc": Trainers = Trainers.OrderByDescending(x => x.HireDate).ToList(); break;
+                case "isAvailable": Trainers = Trainers.OrderBy(x => x.isAvailable).ToList();break;
+                case "isAvailableDesc": Trainers = Trainers.OrderByDescending(x => x.isAvailable).ToList();break;
+    
+            }
+
+            //Pagination
 
 
             return View(Trainers);
